2022239c2be15a24a2f98909140f5939beb2c9a8
nicola00 <nicolajobs00@gmail.com>
2023-03-07 14:52:58 +0100

    START

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script >
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Header,
+    Main,
+  }
+}
+
+</script>
+
+<template>
+  <Header />
+  <Main />
+</template>
+
+<style lang="scss">
+@use './style/gneral.scss';
+</style>
